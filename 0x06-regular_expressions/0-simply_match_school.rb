81231752bb6f34a23b6c4e98092eef9be32a6cd8
KIHALAya <kihalaya00@gmail.com>
2023-11-28 11:51:03 -0800

    Task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-# Ruby script that accepts one argument
-# and pass it to a regular expression matching method
-
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-else
-  input = ARGV[0]
-  pattern = /School/
-
-  if match_data = input.match(pattern)
-    puts match_data[0]
-  else
-    puts
-  end
-end
-
+puts ARGV[0].scan(/School/).join
