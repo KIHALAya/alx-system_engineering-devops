adfd6a17e59b8fcd5acd2e310d95b81975d30b0c
KIHALAya <kihalaya00@gmail.com>
2024-03-12 10:38:40 +0000

    Nginx too many files error fixed

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,15 @@
+#Puppet script to modify  Nginx configuration to increase
+#the maximum number of open file descriptors (worker_rlimit_nofile)
+
+# Fix problem of high amount of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
